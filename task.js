e4fad9df093ac3ae2f31a134c98b5e694dcd942b
ANANDANBAZHAGAN <140066119+ANANDANBAZHAGAN@users.noreply.github.com>
2023-08-31 17:38:08 +0530

    condition

---
+++ task.js
@@ -0,0 +1,170 @@
+// // // NUmber is even or odd
+// // var number = 10
+// // if (number%2 == 0){
+// //     console.log(number,"Is a even number")
+// // }
+// // else{
+// //     console.log("Is a odd number")
+// // }
+
+// // // Compare two number and display the largest number
+// // var Num1 = 10
+// // var Num2 = 20
+// // if (Num1 >= Num2) {
+// //     console.log(Num1, "Largest number")
+// // }
+// // else{console.log(Num1,"smallest number" )}
+
+// // // Calculate grade
+// // var Total = prompt("Enter your number")
+ 
+// //  if (Total>=90 && Total<= 100) {
+// //     console.log("A grade")
+// // }
+// // else if (Total>=80 && Total<90) {
+// //     console.log("B grade")
+// // }
+// // else if (Total>=70 && Total<60) {
+// //     console.log("C grade")
+// // }
+// // else if (Total>=60 && Total< 50) {
+// //     console.log("D grade")
+// // }
+// // else if (Total>=50 && Total< 35) {
+// //     console.log("E grade")
+// // }
+// // else if(Total>=0 && Total<= 35) {
+// //     console.log("F grade")
+// // }+
+// // else{
+// //     console.log("fail")
+// // }
+
+
+
+
+
+// let fruits = "aaaa"
+// switch
+// (fruits) {
+// case
+//  "Banana":
+//     console.log("true") 
+// case
+//  "Apple":
+//     console.log("false")
+//  default:
+//         console.log("Nothing")
+// }
+
+// let a=1;
+// switch(a){
+//     case 1:
+//         a = "one";
+//         break;
+//     case 2:
+//         a = "two";
+//         break;
+//     default:
+//         a = "nothing";
+//         break;
+// }
+// console.log(`The value is ${a}`)
+
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//         result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         break;
+//     case '-':
+//         result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+//     case '*':
+//         result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+//     case '/':
+//         result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+
+// let Name = 'Anand';
+// switch(Name) {
+//     case 'Anand':
+//     case 'Deva':
+//     case 'mohan':
+//         console.log(`${Name} is a Name`);
+//         break;
+//     default:
+//         console.log(`${Name} is not a Name.`);
+//         break;
+// }
+
+
+// var number = prompt("Enter your number")
+// var check = (number % 2 == 0) ? "Even" : "Odd"
+// console.log(`The number is ${check}`);
+
+// var a = 0;
+// var value = (a >= 0) ? (a == 0 ? "Zero" : "Positive") : "Negative"
+// console.log(`The number is ${value}`)
+
+// let marks = 90;
+//     let result = (marks < 40) ? "bad" :
+//         (marks < 60) ? "Average" :
+//             (marks < 80) ? "Good" : "Excellent";
+ 
+//     console.log(result);
+
+// var Number = 15;
+// for (let i = 1; i < Number; i++){
+//     Sum +=i
+//     console.log(Number)
+// }
+
+
+
+var n = 15;
+for (let i = 1; i < n; i++){
+    console.log(i)
+}
+
+
+
+var o = 29;
+for (let i = 6; i < 29; i++){
+    if (i % 2 != 0) 
+        console.log(i)
+}
+
+
+
+var E = 40
+for (let i = 20; i < 40; i++){
+    if (i % 2 == 0)
+         console.log(i)
+
+}
+
+var z = 45
+for (i = 1; i <= 45; i++){
+    if (i % 3 == 0|| i % 5 == 0)
+    console.log(i)
+}
+
+
+
+        
+
+  
